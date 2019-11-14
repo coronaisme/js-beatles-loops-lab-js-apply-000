c016af9697c4b8d29844f235aa328557d45daf3f
Nick Corona <coronaisme@gmail.com>
2019-11-14 19:17:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++){
+   array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var i = 0;
+  while(facts[i]){
+   array.push(facts[i] + "!!!");
+   i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  var string = 'I love the Beatles!';
+  var i = 0;
+  
+  do {
+    array.push(string);
+    i = number;
+  }
+  while (i < 15);
+  return array;
+}
+
+
+
+
+
+
+
+
